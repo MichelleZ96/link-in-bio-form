07015ae49020bc166637750bb4e94716ccbadd2f
MichelleZ96 <michelle.zhang@ChicagoBooth.edu>
2024-11-09 16:03:17 +0000

    Added url and form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,20 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def form
+    render({ :template => "item_templates/form"})
+  end
+
+  def save_item
+    x = Item.new
+    x.link_url = params.fetch("link_url_param")
+    x.link_description = params.fetch("link_description_param")
+    x.thumbnail_url = params.fetch("thumbnail_url_param")
+    x.save
+
+    redirect_to("/")
+  end
+
+
 end
